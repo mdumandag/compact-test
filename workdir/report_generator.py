c375db11302ea13a014ff69e690ef6c74d1ca97d
mdumandag <metin@hazelcast.com>
2021-12-03 14:48:21 +0300

    fix report generation for query benchmarks

--- workdir/report_generator.py
+++ workdir/report_generator.py
@@ -19,23 +19,28 @@ TESTS = [
     "old_engine_query",
 ]
 
-TEST_TYPES = [
-    "throughput",
-    "latency"
-]
+TEST_TYPES = ["throughput", "latency"]
 
 
 def generate_report(test_name, sizes, kinds):
     for size in sizes:
         for test_type in TEST_TYPES:
             folders = [
-                f"{test_name}_{kind}_{size}_{test_type}" for kind in kinds
+                f"{test_name}_{kind}_{size + '_' if size else ''}{test_type}"
+                for kind in kinds
             ]
 
-            subprocess.run(["benchmark-report", "-o", f"reports/{test_name}_{size}_{test_type}", *folders])
+            subprocess.run(
+                [
+                    "benchmark-report",
+                    "-o",
+                    f"reports/{test_name}_{size + '_' if size else ''}{test_type}",
+                    *folders,
+                ]
+            )
 
 
 generate_report("write", OBJECT_SIZES, OBJECT_KINDS)
 generate_report("read", OBJECT_SIZES, OBJECT_KINDS)
-generate_report("query", OBJECT_SIZES[:-1], OBJECT_KINDS[:-1])
+generate_report("query", [""], OBJECT_KINDS[:-1])
 generate_report("old_engine_query", OBJECT_SIZES, OBJECT_KINDS[:-1])
